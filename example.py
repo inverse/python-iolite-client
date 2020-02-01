47a6e2e7bdac8fa8b4a0c191609d6b158cd0298f
Malachi Soord <inverse.chi@gmail.com>
2020-02-01 21:13:40 +0100

    Prep for package

--- example.py
+++ example.py
@@ -1,9 +1,14 @@
+import logging
 import os
+import time
 
 from environs import Env
 
 from iolite.client import IOLiteClient
 from iolite.oauth_handler import OAuthHandler, OAuthStorage, OAuthWrapper
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 env = Env()
 env.read_env()
@@ -13,11 +18,21 @@ PASSWORD = os.getenv('PASSWORD')
 CODE = os.getenv('CODE')
 NAME = os.getenv('NAME')
 
-
+# Get SID
 oauth_storage = OAuthStorage('.')
 oauth_handler = OAuthHandler(USERNAME, PASSWORD)
 oauth_wrapper = OAuthWrapper(oauth_handler, oauth_storage)
 sid = oauth_wrapper.get_sid(CODE, NAME)
 
+# Init client
 client = IOLiteClient(sid, USERNAME, PASSWORD)
+
+logger.info('Connecting to client')
+
 client.connect()
+
+while not client.finished_discovery:
+    time.sleep(1)
+    logger.info('sleeping')
+
+logger.info('Finished discovery')
