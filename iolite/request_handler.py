6e814e04a3c610f71d2671b92a740634e82aa9ee
Malachi Soord <inverse.chi@gmail.com>
2020-08-02 22:34:25 +0200

    Change random generation

--- iolite/request_handler.py
+++ iolite/request_handler.py
@@ -1,4 +1,3 @@
-import random
 import string
 import time
 from enum import Enum
@@ -79,9 +78,8 @@ class RequestHandler:
         return request
 
     def _get_request_id(self, prefix: str) -> str:
-        letters = string.ascii_letters
         while True:
-            request_id = ''.join(random.choice(letters) for i in range(10))
+            request_id = ''.join(secrets.choice(string.ascii_letters) for i in range(10))
             request_id = f'{prefix}_{request_id}'
             if request_id not in self.request_stack:
                 return request_id
