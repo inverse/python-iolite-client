74601ee648a24cc0f6389e46e06800d0ed2b61e3
Malachi Soord <inverse.chi@gmail.com>
2021-07-30 21:35:35 +0200

    Fix up version

--- setup.py
+++ setup.py
@@ -1,23 +1,13 @@
-import os
-
 from setuptools import setup
 
-
-def read_version(package):
-    with open(os.path.join(package, "__init__.py"), "r") as fd:
-        for line in fd:
-            if line.startswith("__version__ = "):
-                return line.split()[-1].strip().strip("'")
-
-
-VERSION = read_version("iolite")
+from iolite import __version__
 
 setup(
     description="API client for interacting with IOLite's remote API",
     author="Malachi Soord",
     author_email="me@malachisoord.com",
     url="https://github.com/inverse/python-iolite-client",
-    version=VERSION,
+    version=__version__,
     install_requires=["requests", "websockets", "environs"],
     test_requires=["pylama", "pytest"],
     name="iolite",
