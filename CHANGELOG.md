2209e9fbec5cc24f7997f03688baee43a54fc244
Malachi Soord <inverse.chi@gmail.com>
2021-10-12 23:49:44 +0200

    Async OAuthHandler + tidy deps (#97)
    * Async OAuthHandler
    
    * Tidy deps
    
    * Fix coverage
    
    * Tests
    
    * Fix
    
    * rem

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+### Added
+
+-   Add `AsyncOAuthHandler`
+
 ### Fixed
 
 -   Fix type on `valve_position` on `RadiatorValve`
--- test/test_oauth_handler.py
+++ test/test_oauth_handler.py
@@ -1,12 +1,22 @@
+import asyncio
 import datetime
+import json
 import unittest
 from unittest.mock import Mock
 
+import aiohttp
+import pytest
 import responses
+from aioresponses import aioresponses
 from freezegun import freeze_time
 from requests import HTTPError
 
-from iolite_client.oauth_handler import OAuthHandler, OAuthWrapper
+from iolite_client.oauth_handler import (
+    AsyncOAuthHandler,
+    OAuthHandler,
+    OAuthHandlerHelper,
+    OAuthWrapper,
+)
 
 
 class OAuthHandlerTest(unittest.TestCase):
@@ -33,6 +43,56 @@ class OAuthHandlerTest(unittest.TestCase):
         oauth_handler = OAuthHandler("user", "password")
         response = oauth_handler.get_access_token("real-code", "my-device")
         self.assertIsInstance(response, dict)
+
+
+class AsyncOAuthHandlerTest(unittest.TestCase):
+    @staticmethod
+    def _run_async(callback):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(callback())
+
+    @pytest.mark.enable_socket
+    def test_get_access_token_invalid_credentials(self):
+
+        with aioresponses() as m:
+            m.post("https://remote.iolite.de/ui/token", status=403)
+
+            async def async_callback():
+                async with aiohttp.ClientSession() as web_session:
+                    oauth_handler = AsyncOAuthHandler("user", "password", web_session)
+                    with self.assertRaises(
+                        aiohttp.client_exceptions.ClientConnectionError
+                    ):
+                        await oauth_handler.get_access_token("dodgy-code", "my-device")
+
+            self._run_async(async_callback)
+
+    @pytest.mark.enable_socket
+    def test_get_access_token_valid(self):
+        with aioresponses() as m:
+            query = OAuthHandlerHelper.get_access_token_query("real-code", "my-device")
+            m.post(
+                f"https://remote.iolite.de/ui/token?{query}",
+                body=json.dumps(
+                    {
+                        "access_token": "token",
+                        "refresh_token": "refresh-token",
+                        "token_type": "BEARER",
+                        "expires_in": 604799,
+                        "expires_at": 1596878461.13904,
+                    }
+                ),
+            )
+
+            async def async_callback():
+                async with aiohttp.ClientSession() as web_session:
+                    oauth_handler = AsyncOAuthHandler("user", "password", web_session)
+                    response = await oauth_handler.get_access_token(
+                        "real-code", "my-device"
+                    )
+                    self.assertIsInstance(response, dict)
+
+            self._run_async(async_callback)
 
 
 class OAuthWrapperTest(unittest.TestCase):
