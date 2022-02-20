01111def0797268558d51e72dd323da3c30a34ec
Malachi Soord <inverse.chi@gmail.com>
2022-02-20 21:26:21 +0100

    Tidy tests

--- test/test_oauth_handler.py
+++ test/test_oauth_handler.py
@@ -1,4 +1,3 @@
-import asyncio
 import datetime
 import json
 import unittest
@@ -46,29 +45,20 @@ class OAuthHandlerTest(unittest.TestCase):
 
 
 class AsyncOAuthHandlerTest(unittest.TestCase):
-    @staticmethod
-    def _run_async(callback):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(callback())
-
+    @pytest.mark.asyncio
     @pytest.mark.enable_socket
-    def test_get_access_token_invalid_credentials(self):
-
+    async def test_get_access_token_invalid_credentials(self):
         with aioresponses() as m:
             m.post("https://remote.iolite.de/ui/token", status=403)
 
-            async def async_callback():
-                async with aiohttp.ClientSession() as web_session:
-                    oauth_handler = AsyncOAuthHandler("user", "password", web_session)
-                    with self.assertRaises(
-                        aiohttp.client_exceptions.ClientConnectionError
-                    ):
-                        await oauth_handler.get_access_token("dodgy-code", "my-device")
+            async with aiohttp.ClientSession() as web_session:
+                oauth_handler = AsyncOAuthHandler("user", "password", web_session)
+                with self.assertRaises(aiohttp.client_exceptions.ClientConnectionError):
+                    await oauth_handler.get_access_token("dodgy-code", "my-device")
 
-            self._run_async(async_callback)
-
+    @pytest.mark.asyncio
     @pytest.mark.enable_socket
-    def test_get_access_token_valid(self):
+    async def test_get_access_token_valid(self):
         with aioresponses() as m:
             query = OAuthHandlerHelper.get_access_token_query("real-code", "my-device")
             m.post(
@@ -84,15 +74,12 @@ class AsyncOAuthHandlerTest(unittest.TestCase):
                 ),
             )
 
-            async def async_callback():
-                async with aiohttp.ClientSession() as web_session:
-                    oauth_handler = AsyncOAuthHandler("user", "password", web_session)
-                    response = await oauth_handler.get_access_token(
-                        "real-code", "my-device"
-                    )
-                    self.assertIsInstance(response, dict)
-
-            self._run_async(async_callback)
+            async with aiohttp.ClientSession() as web_session:
+                oauth_handler = AsyncOAuthHandler("user", "password", web_session)
+                response = await oauth_handler.get_access_token(
+                    "real-code", "my-device"
+                )
+                self.assertIsInstance(response, dict)
 
 
 class OAuthWrapperTest(unittest.TestCase):
@@ -105,27 +92,14 @@ class OAuthWrapperTest(unittest.TestCase):
 
     @freeze_time("2021-01-01 00:00:00")
     def test_get_sid_valid_access_token(self):
-        self.oauth_wrapper.get_sid(
-            {
-                "expires_at": datetime.datetime(2021, 1, 1, 0, 0, 1).timestamp(),
-                "access_token": "access-token",
-            }
-        )
+        token = self._get_token(datetime.datetime(2021, 1, 1, 0, 0, 1))
+        self.oauth_wrapper.get_sid(token)
         self.mock_oauth_handler.get_sid.assert_called_once_with("access-token")
 
     @freeze_time("2021-01-01 00:00:01")
     def test_get_sid_expired_access_token(self):
-        token = {
-            "expires_at": datetime.datetime(2021, 1, 1, 0, 0, 0).timestamp(),
-            "access_token": "access-token",
-            "refresh_token": "refresh-token",
-        }
-
-        response = {
-            "expires_at": datetime.datetime(2021, 1, 10, 0, 0, 0).timestamp(),
-            "access_token": "access-token",
-        }
-
+        token = self._get_token(datetime.datetime(2021, 1, 1, 0, 0, 0))
+        response = self._get_token(datetime.datetime(2021, 1, 10, 0, 0, 0))
         self.mock_oauth_handler.get_new_access_token.return_value = response
 
         self.oauth_wrapper.get_sid(token)
@@ -138,12 +112,7 @@ class OAuthWrapperTest(unittest.TestCase):
     @freeze_time("2021-01-01 00:00:00")
     def test_invalid_token_refresh(self):
 
-        token = {
-            "expires_at": datetime.datetime(2021, 1, 1, 0, 0, 1).timestamp(),
-            "access_token": "access-token",
-            "refresh_token": "refresh-token",
-        }
-
+        token = self._get_token(datetime.datetime(2021, 1, 1, 0, 0, 1))
         self.mock_oauth_storage.fetch_access_token.return_value = token
 
         self.mock_oauth_handler.get_sid.side_effect = [
@@ -151,10 +120,7 @@ class OAuthWrapperTest(unittest.TestCase):
             "sid",
         ]
 
-        response = {
-            "expires_at": datetime.datetime(2021, 1, 10, 0, 0, 0).timestamp(),
-            "access_token": "access-token",
-        }
+        response = self._get_token(datetime.datetime(2021, 1, 10, 0, 0, 0))
 
         self.mock_oauth_handler.get_new_access_token.return_value = response
 
@@ -162,3 +128,11 @@ class OAuthWrapperTest(unittest.TestCase):
         oauth_wrapper.get_sid(token)
 
         self.assertEqual(self.mock_oauth_handler.get_sid.call_count, 2)
+
+    @staticmethod
+    def _get_token(date_time: datetime.datetime) -> dict:
+        return {
+            "expires_at": date_time.timestamp(),
+            "access_token": "access-token",
+            "refresh_token": "refresh-token",
+        }
